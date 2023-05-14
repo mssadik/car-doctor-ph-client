39fbcd936a519e42a1c4020bb5e76335e3cef69f
Sadik <anarul6777@gmail.com>
2023-05-14 15:35:01 +0600

    first commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,22 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:import.meta.env.VITE_apiKey,
+//   authDomain:import.meta.env.VITE_authDomain,
+//   projectId:import.meta.env.VITE_projectId,
+//   storageBucket:import.meta.env.VITE_storageBucket,
+//   messagingSenderId:import.meta.env.VITE_messagingSenderId,
+//   appId:import.meta.env.VITE_appId
+// };
+
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_apiKey,
-  authDomain:import.meta.env.VITE_authDomain,
-  projectId:import.meta.env.VITE_projectId,
-  storageBucket:import.meta.env.VITE_storageBucket,
-  messagingSenderId:import.meta.env.VITE_messagingSenderId,
-  appId:import.meta.env.VITE_appId
+  apiKey: "AIzaSyAeVJgkuOboS0nBX5hMRQ127uTTCYATmRE",
+  authDomain: "car-doctor-4cf1e.firebaseapp.com",
+  projectId: "car-doctor-4cf1e",
+  storageBucket: "car-doctor-4cf1e.appspot.com",
+  messagingSenderId: "870398370356",
+  appId: "1:870398370356:web:d760448dea56ae0059e061"
 };
 
 // Initialize Firebase
